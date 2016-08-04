cda1da661924460381c13fc658f168393a3f9020
Vadym Maksyuk <maksyukvadim@gmail.com>
2016-08-04 17:39:53 +0300

    fix bugs with positioning first button

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
 
 
 $(document).ready(function () {
+        var heig = $(window).height();
+        var widthNew = $(window).width();
+
+        if(widthNew > 768) {
+        $(window).resize(function(){
+            positionElementFirstWindow ();    
+            });
+        }
+   $('.background').css({"height":heig});
+        
+    function positionElementFirstWindow () {
+            var heig = $(window).height();
+            var width = $(window).width();
+            $('#Introducing').css({"marginTop":heig});
+            $('#header').css({"height":heig});
+            $('.maint-text-block').css({"marginTop":(heig/10) });
+            $('.caption-text-bclok').css({"marginTop":(heig/50) });
+            $('.blockBtn').css({"marginTop":(heig/5) });
+            $('#header .row:first-child').css({"marginTop":(heig/20)});
+            $('.maint-text-head').css({"fontSize":((heig/30+width/45)) });
+            $('.caption-text').css({"fontSize":(heig/50)});
+
+    }
+
+    positionElementFirstWindow ();
+  
+    
     $(".top-menu").on("click", "a", function (event) {
         event.preventDefault();
         var id = $(this).attr('href'),
@@ -47,10 +74,17 @@ $(document).ready(function () {
         }, 200);
 
     }
+    if(widthNew < 768) {
 
-    $('#bs-example-navbar-collapse-1 a' ).click(function(){
+        $('#bs-example-navbar-collapse-1 a' ).click(function(){
             $('#btnMenu').trigger('click'); 
     });
+    }
+    
+    $(".navbar").click(function(){   
+  $(".top-menu").toggle();
+});
+
 
     $('#carousel-example-generic').carousel({interval: 5000});
 
@@ -81,6 +115,10 @@ $(document).ready(function () {
 		
 		
 	});	
+
+   
+
+
 });
 
 
